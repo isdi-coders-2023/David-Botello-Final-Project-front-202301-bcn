ffc7a97ef4851d5281119619fa405fde1e52ef0e
David Botello <dabocher@gmail.com>
2023-03-13 13:39:39 +0100

    Input component styles

---
+++ src/components/Form/Input/InputStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const InputStyled = styled.div`
+  .form__input {
+    width: 100%;
+    font-family: var(--secondary-font);
+    height: 5rem;
+    box-shadow: 0 4px 4px (0, 0, 0, 0.25);
+    padding: 1rem;
+    border-radius: 4px;
+  }
+`;
+
+export default InputStyled;
