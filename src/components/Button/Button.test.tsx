af75b0bca93aae66c119b3e8c8f02569f5d0487a
David Botello <dabocher@gmail.com>
2023-03-17 13:46:03 +0100

    Add disabled case to test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,11 +4,35 @@ import Button from "./Button";
 describe("Given a Button component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a button", () => {
-      render(<Button />);
+      render(<Button text={""} isDisabled={false} />);
 
       const expectedButtonElement = screen.getByRole("button");
 
       expect(expectedButtonElement).toBeInTheDocument();
+    });
+
+    describe("When it is rendered with text 'Iniciar'", () => {
+      test("Then it should show a button with text 'Iniciar'", () => {
+        const text = "Iniciar";
+        const expectedText = "Iniciar";
+        render(<Button text={text} isDisabled={false} />);
+
+        const expectedButtonText = screen.getByRole("button");
+
+        expect(expectedButtonText).toHaveTextContent(expectedText);
+      });
+    });
+
+    describe("When it is rendered with props 'isDisabled=true'", () => {
+      test("Then it should show a button disabled", () => {
+        const isDisabled = true;
+
+        render(<Button text={""} isDisabled={isDisabled} />);
+
+        const expectedButton = screen.getByRole("button");
+
+        expect(expectedButton).toBeDisabled();
+      });
     });
   });
 });
